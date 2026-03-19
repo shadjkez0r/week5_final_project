17023fd8b2c65bf298b69d3d170c8ade5428513b
dolgeim976 <dolgeim976@example.com>
2026-03-19 16:43:16 +0400

    Реализация алгоритмов сортировки: пузырьком и вставками.

--- src/main/java/team4/finalproject/strategy/BubbleSortStrategy.java
+++ src/main/java/team4/finalproject/strategy/BubbleSortStrategy.java
@@ -6,6 +6,18 @@ import java.util.List;
 public class BubbleSortStrategy<T> implements SortingStrategy<T> {
     @Override
     public void sort(List<T> list, Comparator<T> comparator) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - 1; j++) {
+                if (comparator.compare(list.get(j), list.get(j + 1)) > 0) {
+                    swap(j, j + 1, list);
+                }
+            }
+        }
+    }
 
+    private void swap(int x, int y, List<T> list) {
+        T temp = list.get(x);
+        list.set(x, list.get(y));
+        list.set(y, temp);
     }
 }
--- src/main/java/team4/finalproject/strategy/SelectionSortStrategy.java
+++ src/main/java/team4/finalproject/strategy/SelectionSortStrategy.java
@@ -6,6 +6,15 @@ import java.util.List;
 public class SelectionSortStrategy<T> implements SortingStrategy<T> {
     @Override
     public void sort(List<T> list, Comparator<T> comparator) {
+        for (int i = 1; i < list.size(); i++) {
+            T current = list.get(i);
+            int x = i - 1;
 
+            while (x >= 0 && comparator.compare(current, list.get(x)) > 0) {
+                list.set(x + 1, list.get(x));
+                x--;
+            }
+            list.set(x + 1, current);
+        }
     }
 }
