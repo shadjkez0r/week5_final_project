876c142de7470be660dd2745327b5c02e6d81f03
evg <shadjkez0r48@gmail.com>
2026-03-23 22:44:10 +0400

    feat: release even natural sort order

---
+++ src/main/java/team4/finalproject/service/strategy/EvenOnlySortStrategy.java
@@ -0,0 +1,38 @@
+package team4.finalproject.service.strategy;
+
+import team4.finalproject.domain.Student;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.ToIntFunction;
+
+public class EvenOnlySortStrategy implements SortingStrategy<Student> {
+    private final SortingStrategy<Student> delegate;
+    private final ToIntFunction<Student> fieldForNaturalOrder;
+
+    public EvenOnlySortStrategy(SortingStrategy<Student> delegate, ToIntFunction<Student> fieldForNaturalOrder) {
+        this.delegate = delegate;
+        this.fieldForNaturalOrder = fieldForNaturalOrder;
+    }
+
+    @Override
+    public void sort(List<Student> list, Comparator<Student> comparator) {
+        List<Integer> evenIndicies = new ArrayList<>();
+        List<Student> evenElements = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            if (fieldForNaturalOrder.applyAsInt(list.get(i)) % 2 == 0) {
+                evenIndicies.add(i);
+                evenElements.add(list.get(i));
+            }
+        }
+
+        delegate.sort(evenElements, comparator);
+
+        for (int i = 0; i < evenIndicies.size(); i++) {
+            list.set(evenIndicies.get(i), evenElements.get(i));
+        }
+
+    }
+}
